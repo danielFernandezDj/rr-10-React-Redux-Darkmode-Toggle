8a649a751372096b8f0321f77f9c36f462ac3eb6
Daniel Fernandez <mdanif9728@gmail.com>
2024-06-24 20:03:55 -0700

    first commit

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from "react-dom/client";
 import App from './App';
 
-ReactDOM.render(
+// Get the root element from the DOM
+const container = document.getElementById('root')
+
+// Create a root
+const root = createRoot(container)
+
+// Render the app
+root.render(
   <React.StrictMode>
-      <App />
+    <App />
   </React.StrictMode>,
-  document.getElementById('root')
 );
